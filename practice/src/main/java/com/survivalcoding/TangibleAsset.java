e856c063f01f0f0541d8f2bb0996d3eaf9146d47
Seungbin.Park <waterdog0601@naver.com>
2026-06-22 14:49:24 +0900

    연습문제4

--- practice/src/main/java/com/survivalcoding/TangibleAsset.java
+++ practice/src/main/java/com/survivalcoding/TangibleAsset.java
@@ -1,12 +1,24 @@
 package com.survivalcoding;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
     int price;
     String color;
+
+    double weight;
 
     TangibleAsset(String name, int price, String color) {
         super(name);
         this.price = price;
         this.color = color;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 }
